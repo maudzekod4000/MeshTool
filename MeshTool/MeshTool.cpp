3a45a06d0caff2a94c0df2a4c540adbc86111177
quenchaman <valericfbg@gmail.com>
2022-09-30 14:40:26 +0300

    Running rapidjson

--- MeshTool/MeshTool.cpp
+++ MeshTool/MeshTool.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+#include "lib/rapidjson-1.1.0/rapidjson.h"
+#include "lib/rapidjson-1.1.0/writer.h"
+#include "lib/rapidjson-1.1.0/stringbuffer.h"
+#include "lib/rapidjson-1.1.0/document.h"
+
+using namespace rapidjson;
+
 int main()
 {
-    std::cout << "Hello Mesh!\n";
+  // 1. Parse a JSON string into DOM.
+  const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+  Document d;
+  d.Parse(json);
+
+  // 2. Modify it by DOM.
+  Value& s = d["stars"];
+  s.SetInt(s.GetInt() + 1);
+
+  // 3. Stringify the DOM
+  StringBuffer buffer;
+  Writer<StringBuffer> writer(buffer);
+  d.Accept(writer);
+
+  // Output {"project":"rapidjson","stars":11}
+  std::cout << buffer.GetString() << std::endl;
+  return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MeshTool
 A tool for analysing and calculating data about meshes
+
+Build with Windows SDK 10, C++ 14 and Visual Studio 2019 (v142)
